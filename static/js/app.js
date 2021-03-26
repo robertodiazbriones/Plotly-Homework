8d4707b944794d479bbde6cdfffd845145a0d6a1
unknown <robertodiazbriones@gmail.com>
2021-03-25 21:12:25 -0400

    Get the Selector ID Working

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,30 @@
 
-d3.json("data/samples.json").then(function(data){
-     console.log(data);
-     });
+var dropdownmenu = d3.select('#selDataset');
+
+d3.json("data/samples.json").then(function (data) {
+
+    var data_names = data.names;
+    
+    data_names.forEach(function (name) {
+      dropdownmenu
+          .append("option")
+          .text(name)
+          .property("value", name);
+    });
+
+    //let selectedID = "940";
+
+    //datapull(selectedID);
+  });
+
+
+
+// // Create Dynamic changes on Dropdowm button
+// function optionChanged(value){
+
+//      MetaDataSample(value);
+//      PieChart(value);
+//      BubbleChart(value);
+//      GaugeChart(value);
+ 
+//  }
